2002fb2e60b4ff68507112874c569a0c91186332
SOH-BRYKLINE <sohbrykline3@gmail.com>
2025-08-13 05:42:42 -0500

    flask api

--- Backend/api.py
+++ Backend/api.py
@@ -16,6 +16,23 @@ app = Flask(
 @app.route('/')
 def index():
     return render_template('index.html')
-
+@app.route('/about')
+def about():
+    return render_template('about.html')
+@app.route('/features')
+def features():
+    return render_template('features.html')
+@app.route('/explore')
+def explore():
+    return render_template('explore.html')
+@app.route('/blog')
+def blog():
+    return render_template('blog.html')
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+@app.route('/login')
+def login():
+    return render_template('login.html')
 if __name__ == '__main__':
     app.run(debug=True)
